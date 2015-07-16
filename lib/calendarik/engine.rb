53cc29850b0627c3ae928261dc7a8270e7f2590c
Igor Biryukov <balalaukin@gmail.com>
2015-07-16 16:39:50 +0300

    upload decorators directory in rails app

--- lib/calendarik/engine.rb
+++ lib/calendarik/engine.rb
@@ -4,5 +4,10 @@ require "momentjs-rails"
 
 module Calendarik
   class Engine < Rails::Engine
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
